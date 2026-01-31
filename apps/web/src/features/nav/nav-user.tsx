5efad71be3d4d97dc3b5eb374dd042f2d3b27d6c
Natip85 <106977567+Natip85@users.noreply.github.com>
2026-01-31 18:15:58 +0200

    fix

--- apps/web/src/features/nav/nav-user.tsx
+++ apps/web/src/features/nav/nav-user.tsx
@@ -67,7 +67,7 @@ export function NavUser() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-        side="right"
+        side="top"
         align="end"
         sideOffset={4}
       >
